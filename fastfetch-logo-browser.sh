35c9ad6f96ed09629cb8de4bc3d32f0e5683f293
a5xwin <ashmercesletifercoc@gmail.com>
2025-08-07 11:44:01 +0530

    Initial commit: added logo browser script, license, and readme

---
+++ fastfetch-logo-browser.sh
@@ -0,0 +1,115 @@
+logos=(
+"Ada" "Adélie" "Adelie" "aerOS" "Afterglow" "aix" "Almalinux" "Alpine" "Alpine2" "Alpine_small" "alpine2_small"
+"alpine3_small" "Alter" "ALTLinux" "Amazon" "Amazon Linux" "amzn" "AmogOS" "Anarchy" "android" "android_small"
+"anduinos" "Antergos" "antiX" "Aosc OS/Retro" "aoscosretro" "Aosc OS/Retro_small" "aoscosretro_small" "Aosc OS"
+"aoscos" "Aosc OS_old" "aoscos_old" "Aperture" "Apple" "Apple_small" "Apricity" "ArchBox" "Archcraft" "Archcraft2"
+"arch" "archmerge" "arch2" "arch3" "arch_small" "arch_old" "ARCHlabs" "ArchStrike" "Arkane" "Arkane Linux"
+"Armbian" "Armbian2" "artix" "artixlinux" "artix-linux" "artix_small" "artixlinux_small" "artix2_small"
+"artixlinux2_small" "arco" "arcolinux" "arco-linux" "arco_small" "arcolinux_small" "arco-linux_small" "arse"
+"arselinux" "arse-linux" "Arya" "asahi" "asahi-linux" "asahi2" "asahi-linux2" "aster" "AsteroidOS" "astOS"
+"Astra" "Astra Linux" "astralinux" "Ataraxia Linux" "Ataraxia" "AthenaOS" "AthenaOS_old" "Aurora" "Azos"
+"bedrock" "bedrock_small" "BigLinux" "Bitrig" "Blackarch" "BlackMesa" "black-mesa" "BlackPanther" "BLAG"
+"BlankOn" "BlueLight" "Bodhi" "Bonsai" "Bredos" "BSD" "BunsenLabs" "CachyOS" "CachyOS_small" "Calculate"
+"Calinix" "calinixos" "Calinix_small" "calinixos_small" "Carbs" "CBL-Mariner" "Cel" "celos" "cel-linux"
+"celos-linux" "Center" "CentOS" "CentOS_small" "Cereus" "Cereus Linux" "Chakra" "ChaletOS" "Chapeau"
+"Chimera" "ChonkySealOS" "Chrom" "ChromeOS" "Cleanjaro" "Cleanjaro_small" "Clear Linux" "clearlinux"
+"Clear Linux OS" "ClearOS" "Clover" "Cobalt" "Codex Linux" "Condres" "ContainerLinux" "Container Linux"
+"Container Linux by CoreOS" "common-torizon" "Cosmic" "CRUX" "CRUX_small" "Crystal" "crystal-linux"
+"Crystal-Linux" "Cucumber" "CucumberOS" "CuerdOS" "CuerdOS GNU/Linux" "CutefishOS" "CuteOS" "CyberOS"
+"cycledream" "dahliaOS" "DarkOS" "Debian" "debian-linux" "Debian_small" "debian-linux_small" "Deepin"
+"deepin-linux" "DesaOS" "Devuan" "Devuan_small" "DietPi" "DracOS" "DragonFly" "DragonFly_small" "DragonFly_old"
+"DraugerOS" "Drauger" "Droidian" "elbrus" "Elementary" "Elementary_small" "Elive" "EncryptOS" "EndeavourOS"
+"EndeavourOS_small" "Endless" "Enso" "EshanizedOS" "EuroLinux" "Evolinx" "EvolutionOS" "EvolutionOS_small"
+"EvolutionOS_old" "eweOS" "Exherbo" "exherbo-linux" "Exodia Predator" "exodia-predator" "Exodia Predator OS"
+"Fastfetch" "FF" "Fedora" "fedora-asahi-remix" "Fedora_small" "Fedora_old" "Fedora_silverblue"
+"fedora-silverblue" "Fedora_kinoite" "fedora-kinoite" "Fedora_sericea" "fedora-sericea" "Fedora_coreos"
+"fedora-coreos" "FemboyOS" "Feren" "filotimo" "Finnix" "Floflis" "Freebsd" "freebsd_small" "FreeMiNT"
+"Frugalware" "frugalware-linux" "Funtoo" "Furreto" "GalliumOS" "Garuda" "garuda-linux" "GarudaDragon"
+"garuda-dragon" "Garuda_small" "garuda-linux_small" "Gentoo" "Gentoo_small" "GhostBSD" "Glaucus" "gNewSense"
+"GNOME" "GNU" "GoboLinux" "Gobo" "GoldenDog Linux" "GDL" "goldendoglinux" "GrapheneOS" "Grombyang" "Guix"
+"Guix_small" "GXDE" "Haiku" "Haiku2" "Haiku_small" "HamoniKR" "HarDClanZ" "HardenedBSD" "Hash" "HeliumOS"
+"Huawei Cloud EulerOS" "hce" "Huayra" "Hybrid" "HydroOS" "hypros" "Hyperbola" "Hyperbola_small" "Hydra"
+"Iglunix" "Iglu" "InstantOS" "Interix" "IRIX" "Ironclad" "Itc" "januslinux" "janus" "Kaisen" "Kali"
+"Kalilinux" "Kali_small" "Kalilinux_small" "KaOS" "KernelOS" "kdelinux" "kde-linux" "KDE Neon" "Kibojoe"
+"KISS" "kiss-linux" "kisslinux" "Kogaion" "Korora" "KrassOS" "Krass" "KSLinux" "Kubuntu" "kubuntu-linux"
+"kde-ubuntu" "Kylin" "kylin" "LainOS" "langitketujuh" "l7" "Laxeros" "LEDE" "LibreELEC" "Lilidog" "Lingmo"
+"LingmoOS" "Linspire" "Lindows" "Linux" "linux-generic" "LinuxFromScratch" "lfs" "Linux_small"
+"linux-generic_small" "LinuxLite" "Linux Lite" "linux_lite" "LinuxLite_small" "Linux Lite_small" "linuxmint"
+"linux-mint" "linuxmint_small" "linux-mint_small" "linuxmint_old" "linux-mint_old" "Live Raizo" "Live_Raizo"
+"LliureX" "LMDE" "locos" "loc-os" "Loc-OS Linux" "lubuntu" "Lunar" "macos" "mac" "macos_small" "mac_small"
+"macos2" "mac2" "macos2_small" "mac2_small" "macos3" "mac3" "MainsailOS" "MainsailOS_small" "Mageia"
+"Mageia_small" "Magix" "MagixOS" "MagpieOS" "Magpie" "mandriva" "mandrake" "manjaro" "manjaro-arm"
+"manjaro_small" "manjaro-arm_small" "MassOS" "mass" "MatuusOS" "Matuus" "MaUI" "Mauna" "Meowix" "Mer"
+"MidnightBSD" "MidOS" "MidOS_old" "Minix" "MIRACLE LINUX" "miracle_linux" "MOS" "Msys2" "MX" "MX Linux"
+"MX_small" "mx linux_small" "MX2" "Namib" "Nekos" "Neptune" "NetRunner" "nexalinux" "Nitrux" "NixOS"
+"NixOS_small" "nixos_old" "nixos_old_small" "NetBSD" "NetBSD2" "NetBSD_small" "nobara" "nobara-linux"
+"nomadbsd" "NurOS" "Nurunner" "NuTyX" "Obarun" "OBRevenge" "OmniOS" "Opak" "openkylin" "open-kylin"
+"openbsd" "openbsd_small" "OpenEuler" "OpenIndiana" "OpenMamba" "OpenStage" "opensuse" "opensuse_small"
+"opensuse-microos" "opensuse-leap" "opensuse-leap_old" "opensuse-tumbleweed" "opensuse-tumbleweed_small"
+"opensuse-tumbleweed_old" "opensuse-slowroll" "opensuse-tumbleweed-slowroll" "openmandriva" "open-mandriva"
+"open_mandriva" "openmandriva lx" "openwrt" "OPNsense" "oracle" "oracle linux" "oracle linux server"
+"orchid" "orchid_small" "Oreon" "OS Elbrus" "OSMC" "Open Source Media Center" "OSX" "OSX_small" "PacBSD"
+"Panwah" "parabola" "parabola-gnulinux" "parabola_small" "parabola-gnulinux_small" "Parch" "Pardus" "Parrot"
+"Parsix" "PCBSD" "TrueOS" "PCLinuxOS" "PearOS" "Pengwin" "Pentoo" "Peppermint" "Peropesis" "Peropesis Linux"
+"PhyOS" "PikaOS" "PisiLinux" "PNM Linux" "pop" "popos" "pop_small" "popos_small" "Porteus" "PostMarketOS"
+"PostMarketOS_small" "Proxmox" "pve" "PuffOS" "Puppy" "PureOS" "PureOS_small" "qts" "Q4OS" "Qubes" "Qubyt"
+"Quibian" "Quirinux" "Radix" "raspbian" "raspi" "raspberrypi" "raspberrypios" "raspbian_small" "raspi_small"
+"raspberrypi_small" "raspberrypios_small" "RavynOS" "RebornOS" "RebornOS_small" "RedCore" "rhel" "redhat"
+"redhat-linux" "rhel_old" "redhat_old" "redhat-linux_old" "RedOS" "RED OS" "red-os" "redos" "RedOS_small"
+"RED OS_small" "red-os_small" "redos_small" "redstar" "redstar-os" "redstaros" "Refracted Devuan"
+"refracted-devuan" "Regata" "Regolith" "RhaymOS" "rocky" "rocky-linux" "rockylinux" "rocky_small"
+"rocky-linux_small" "rockylinux_small" "rosa" "rosa-linux" "rosalinux" "Rhino Linux" "Sabayon" "Sabotage"
+"Sailfish" "SalentOS" "Salient OS" "SalientOS" "Salix" "SambaBOX" "Profelis SambaBOX" "Sasanqua" "Scientific"
+"Serpent OS" "semc" "Septor" "Serene" "SharkLinux" "ShastraOS" "Shebang" "Siduction" "SkiffOS" "SleeperOS"
+"SleeperOS_small" "Slitaz" "SpoinkOS" "spoink-os" "Slackel" "Slackware" "Slackware_small" "SmartOS"
+"SnigdhaOS" "Snigdha" "Soda" "Source Mage" "Source Mage GNU/Linux" "source_mage" "sourcemage" "solaris"
+"sunos" "solaris_small" "sunos_small" "Solus" "solus-linux" "Sparky" "Star" "Stock Linux" "SteamOS"
+"SteamDeck" "SteamDeck_small" "SteamDeckOled" "Sulin" "SummitOS" "suse" "suse-linux" "suse_small"
+"suse-linux_small" "Swagarch" "T2" "T2 SDE" "T2/Linux" "T2_small" "T2 SDE_small" "T2/Linux_small" "Tails"
+"Tatra" "TeArch" "TileOS" "Torizon OS" "TorizonCore" "Trisquel" "Tuxedo OS" "tuxedo" "Twister" "UBLinux"
+"UBLinux_small" "ubuntu" "ubuntu-linux" "ubuntu_small" "ubuntu-linux_small" "ubuntu_old" "ubuntu-linux_old"
+"ubuntu_old2" "ubuntu_old2_small" "ubuntu-budgie" "ubuntu cinnamon" "ubuntu-cinnamon" "ubuntu gnome"
+"ubuntu-gnome" "ubuntu kylin" "ubuntu-kylin" "ubuntu mate" "ubuntu-mate" "ubuntu kde" "ubuntu-kde"
+"ubuntu-plasma" "ubuntu studio" "ubuntu-studio" "ubuntu sway" "ubuntu-sway" "ubuntu touch" "ubuntu-touch"
+"ubuntu unity" "ubuntu-unity" "Ultramarine" "Ultramarine Linux" "Ultramarine_small" "Unifi" "Univalent"
+"Univention" "UOS" "UrukOS" "uwuntu" "Valhalla" "valhallaos" "valhalla-linux" "vanilla" "vanilla2"
+"vanilla_small" "Venom" "Venom_small" "Vnux" "Vzlinux" "void" "void_small" "void2_small" "WiiLinuxNgx"
+"WiiLinux" "Wii-Linux" "Wii Linux" "Windows Server 2025" "Windows 11" "Windows Server 2022" "Windows 11_small"
+"Windows 8" "Windows 8.1" "Windows 10" "Windows Server 2012" "Windows Server 2012 R2" "Windows Server 2016"
+"Windows Server 2019" "Windows" "Windows 7" "Windows Server 2008" "Windows Server 2008 R2" "Windows 95"
+"Windows 9x" "WolfOS" "Xenia" "XCP-ng" "xenenterprise" "Xferience" "XeroArch" "YiffOS" "Zorin" "z/OS" "zos"
+)
+
+index=0
+total=${#logos[@]}
+
+clear_logo() {
+    clear
+}
+
+show_logo() {
+    clear_logo
+    echo -e "🌀 Logo: \033[1;36m${logos[$index]}\033[0m ($((index + 1))/$total)"
+    fastfetch --logo "${logos[$index]}"
+}
+
+while true; do
+    show_logo
+    read -rsn1 key
+    if [[ $key == $'\x1b' ]]; then
+        read -rsn2 -t 0.1 key2
+        case $key2 in
+            "[D") # for left arrow key
+                ((index--))
+                [[ $index -lt 0 ]] && index=$((total - 1))
+                ;;
+            "[C") # for right arrow key
+                ((index++))
+                [[ $index -ge $total ]] && index=0
+                ;;
+        esac
+    elif [[ $key == "q" ]]; then
+        clear
+        exit 0
+    fi
+done
